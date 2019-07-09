0a12df77f0cf5d275d4910eb58b869e0eeaf865e
Josh Isaacson <jsisaacs@iu.edu>
2019-07-09 15:45:56 -0700

    merged with master, added firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaGXSdblvemDImM8XbQYGpCISJRVr0TSY",
+    authDomain: "lmigos.firebaseapp.com",
+    databaseURL: "https://lmigos.firebaseio.com",
+    projectId: "lmigos",
+    storageBucket: "lmigos.appspot.com",
+    messagingSenderId: "957603794853",
+    appId: "1:957603794853:web:b0274d04f8b24ca4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.database();
