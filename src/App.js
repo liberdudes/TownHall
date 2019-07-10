5cd2489c51ef12c71276def47a0d3619a5930ef0
Josh Isaacson <jsisaacs@iu.edu>
2019-07-09 18:16:25 -0700

    set global state, passing props to child comps

--- src/App.js
+++ src/App.js
@@ -4,28 +4,80 @@ import Filter from './components/Filter';
 import Report from './components/Report';
 import Navigation from './components/Navigation';
 import UserFeedbackCard from './components/UserFeedbackCard';
+import NewFeedbackButton from './components/NewFeedbackButton';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Container id="container">
-        <Row>
-          <Col xs={3} id="col1">
-            <Filter/>
-            <Report/>
-          </Col>
-          <Col xs={9} id="col2">
-            <UserFeedbackCard/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      feedback: [
+        {
+          subject: "subject 1",
+          project: "#CIAT",
+          status: "In Progress",
+          description: "desc 1",
+          votes: 21,
+          date: "July 9, 2019"
+        },
+        {
+          subject: "subject 2",
+          project: "#ABC",
+          status: "Done",
+          description: "desc 2",
+          votes: 15,
+          date: "July 3, 2019"
+        },
+        {
+          subject: "subject 3",
+          project: "#CBA",
+          status: "Backlog",
+          description: "desc 3",
+          votes: 15,
+          date: "July 3, 2019"
+        }
+      ]
+    };
+  }
+
+  // let elements = [0, 0, 0];
+  render() {
+    let elements = [0, 0, 0];
+    return (
+      <div className="App">
+        <Navigation/>
+        <Container id="container">
+          <Row>
+            <Col xs={3} id="col1">
+              <Filter/>
+              <Report/>
+            </Col>
+            <Col xs={9} id="col2">
+              <Container>
+                <Row>
+                  <Col xs={7}>
+                  </Col>
+                  <Col xs={5}>
+                    <NewFeedbackButton/>
+                  </Col>
+                </Row>
+              </Container>
+              <ul>
+                {this.state.feedback.map((userFeedback) => {
+                  return <UserFeedbackCard feedback={userFeedback}/>
+                })}
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/UserFeedbackCard.js
+++ src/components/UserFeedbackCard.js
@@ -7,22 +7,37 @@ import Col from 'react-bootstrap/Col';
 
 class Report extends React.Component {
     render() {
+        const status = this.props.feedback.status;
+        let output;
+        if (status === "Backlog") {
+            output = <button id="backlog-badge">
+                        Backlog
+                    </button>
+        }
+        if (status === "In Progress") {
+            output = <button id="in-progress-badge">
+                        In Progress
+                    </button>
+        } 
+        if (status === "Done") {
+            output = <button id="done-badge">
+                        Done
+                    </button>
+        }
         return (
             <div class="user-feedback-card">
                 <Container>
                     <Row>
                         <Col xs={9} id="col1-card">
                             <p id="user-feedback-card-subject">
-                                This is a subject line for feedback from a user
+                                {this.props.feedback.subject}
                             </p>
                             <p id="user-feedback-card-tag">
-                                #CIAT
+                                {this.props.feedback.project}
                             </p>
                         </Col>
                         <Col xs={3} id="col2-card">
-                            <button id="in-progress-badge">
-                                In Progress
-                            </button>
+                            {output}
                         </Col>
                     </Row>
                 </Container>
@@ -31,7 +46,7 @@ class Report extends React.Component {
                     <Row>
                         <Col xs={8} id="col1-sub">
                             <p id="user-feedback-card-description">
-                                Description of user’s bug, as well as a walkthrough of steps to reproduce the issue. This text extends and is a paragraph.
+                                {this.props.feedback.description}
                             </p>
                         </Col>
                         <Col xs={4} id="col2-sub">
@@ -39,7 +54,7 @@ class Report extends React.Component {
                                 USER VOTES
                             </h5>
                             <p id="upvote-count">
-                                21
+                                {this.props.feedback.votes}
                             </p>
                         </Col>
                     </Row>
@@ -53,7 +68,7 @@ class Report extends React.Component {
                         </Col>
                         <Col xs={4}>
                             <p id="user-feedback-card-date">
-                                July 9, 2019
+                                {this.props.feedback.date}
                             </p>
                         </Col>
                     </Row>
