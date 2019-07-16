8fe932f1aec81399190bcc341d38b46e7cc2bad3
Joshua Isaacson <Joshua.Isaacson@libertymutual.com>
2019-07-16 11:53:47 -0400

    added Prettier for automatic code formatting and husky for pre commit formatting checks

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,118 +1,133 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import ModalBoot from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown'
-import * as helper from '../helper';
-import '../styles/NewFeedbackButton.css';
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import ModalBoot from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
+import * as helper from "../helper";
+import "../styles/NewFeedbackButton.css";
 
 class Modal extends Component {
-    constructor(props, context) {
-      super(props, context);
-  
-      this.handleShow = this.handleShow.bind(this);
-      this.handleClose = this.handleClose.bind(this);
-  
-      this.state = {
-        show: false,
-        formBody: "",
-        formSubject: "",
-        formDropdown: "",
-        dropdownOptions: [],
-        projectId: ""
-      };
-    }
+  constructor(props, context) {
+    super(props, context);
 
-    componentDidMount() {
-        this.setState({dropdownOptions: helper.getProjects()});
-    }
-  
-    handleClose() {
-      this.setState({ show: false });
-    }
-  
-    handleShow() {
-      this.setState({ show: true });
-    }
-  
-    handleSubmit(event) {
-        event.preventDefault();
-        helper.addMessageToProject(this.state.projectId, {
-            subject: this.state.formSubject,
-            body: this.state.formBody
-        })
-        this.handleClose()
-    }
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 
-    handleChange = (event) => {
-        this.setState({
-          [event.target.id]: event.target.value
-        });
-    }
-
-    handleChangeDropdown = (event) => {
-        this.setState({
-          projectId: event.target.text
-        });
-    }
-
-    handleChangeSelect = (event) => {
-      this.setState({
-        projectId: event.target.text
-      });
+    this.state = {
+      show: false,
+      formBody: "",
+      formSubject: "",
+      formDropdown: "",
+      dropdownOptions: [],
+      projectId: ""
+    };
   }
-  
-    render() {
 
-        let optionItems = this.state.dropdownOptions.map((option) =>
-            <Dropdown.Item key={option} value={option}>{option}</Dropdown.Item>
-        );
+  componentDidMount() {
+    this.setState({ dropdownOptions: helper.getProjects() });
+  }
 
-        return (
-        <>
-          <Button id="button-new" onClick={this.handleShow}>
-            NEW FEEDBACK
-          </Button>
-  
-          <ModalBoot show={this.state.show} onHide={this.handleClose}>
-            <ModalBoot.Header closeButton>
-              <ModalBoot.Title>New Feedback</ModalBoot.Title>
-            </ModalBoot.Header>
-            <ModalBoot.Body>
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    helper.addMessageToProject(this.state.projectId, {
+      subject: this.state.formSubject,
+      body: this.state.formBody
+    });
+    this.handleClose();
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleChangeDropdown = event => {
+    this.setState({
+      projectId: event.target.text
+    });
+  };
+
+  handleChangeSelect = event => {
+    this.setState({
+      projectId: event.target.text
+    });
+  };
+
+  render() {
+    let optionItems = this.state.dropdownOptions.map(option => (
+      <Dropdown.Item key={option} value={option}>
+        {option}
+      </Dropdown.Item>
+    ));
+
+    return (
+      <>
+        <Button id="button-new" onClick={this.handleShow}>
+          NEW FEEDBACK
+        </Button>
+
+        <ModalBoot show={this.state.show} onHide={this.handleClose}>
+          <ModalBoot.Header closeButton>
+            <ModalBoot.Title>New Feedback</ModalBoot.Title>
+          </ModalBoot.Header>
+          <ModalBoot.Body>
             <Form onSubmit={e => this.handleSubmit(e)}>
+              <Form.Group controlId="formSubject">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control
+                  value={this.state.formSubject}
+                  type="text"
+                  placeholder="Subject"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
 
-                <Form.Group controlId="formSubject">
-                    <Form.Label>Subject</Form.Label>
-                    <Form.Control value={this.state.formSubject} type="text" placeholder="Subject" onChange={this.handleChange}/>
-                </Form.Group>
+              <Form.Group controlId="formBody">
+                <Form.Label>Comment</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  value={this.state.formBody}
+                  rows="3"
+                  placeholder="Comment"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
 
-                <Form.Group controlId="formBody">
-                    <Form.Label>Comment</Form.Label>
-                    <Form.Control as="textarea" value={this.state.formBody}  rows="3" placeholder="Comment" onChange={this.handleChange}/>
-                </Form.Group>
+              <Form.Group controlId="formDropdown">
+                <Form.Label>Select Project</Form.Label>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant="success"
+                    value={this.state.formDropdown}
+                    placeholder="Drop Down"
+                    id="dropdown-basic"
+                    onChange={this.handleChangeSelect}
+                  ></Dropdown.Toggle>
 
-                <Form.Group controlId="formDropdown">
-                    <Form.Label>Select Project</Form.Label>
-                    <Dropdown>
-                    <Dropdown.Toggle variant="success" value={this.state.formDropdown} placeholder="Drop Down" id="dropdown-basic" onChange={this.handleChangeSelect} >
-                    </Dropdown.Toggle>
+                  <Dropdown.Menu onClick={this.handleChangeDropdown}>
+                    {optionItems}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Form.Group>
 
-                    <Dropdown.Menu onClick={this.handleChangeDropdown}>
-                        {optionItems}
-                    </Dropdown.Menu>
-                    </Dropdown>
-                </Form.Group>
-
-                <Button variant="primary" type="submit" >
-                    Submit
-                </Button>
-                
-                </Form>
-            </ModalBoot.Body>
-          </ModalBoot>
-        </>
-      );
-    }
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </ModalBoot.Body>
+        </ModalBoot>
+      </>
+    );
   }
+}
 
-  export default Modal;
+export default Modal;
--- src/helper.js
+++ src/helper.js
@@ -1,76 +1,76 @@
-import {db} from './components/firebase.js';
-import moment from 'moment';
+import { db } from "./components/firebase.js";
+import moment from "moment";
 
 export function getMessage(id) {
-    return db.ref("messages/" + id);
+  return db.ref("messages/" + id);
 }
 
 export function getProjectMessages(projectId) {
-    return db.ref("projects/" + projectId);
+  return db.ref("projects/" + projectId);
 }
 
 export async function getMessages() {
-    let list = [];
-    db.ref("messages").once('value',snapshot => {
-        snapshot.forEach(childsnap => {
-           // if (snapshot.val().status !== 'Closed') {
-                list.push(childsnap.val());
-           // }
-        })
-    })
-    return list
+  let list = [];
+  db.ref("messages").once("value", snapshot => {
+    snapshot.forEach(childsnap => {
+      // if (snapshot.val().status !== 'Closed') {
+      list.push(childsnap.val());
+      // }
+    });
+  });
+  return list;
 }
 
 export function addMessageToProject(projectId, message) {
-    let updates = {};
+  let updates = {};
 
-    let mKey = db.ref('messages').push().key;
-    message["timestamp"] = moment().valueOf();
-    message["status"] = "New";
-    message["upvote"] = 5;
-    message["project"] = projectId;
-    message["messageId"] = mKey;
+  let mKey = db.ref("messages").push().key;
+  message["timestamp"] = moment().valueOf();
+  message["status"] = "New";
+  message["upvote"] = 5;
+  message["project"] = projectId;
+  message["messageId"] = mKey;
 
-    updates['messages/' + mKey] = message;
-    updates['projects/' + projectId + "/" + mKey] = true;
+  updates["messages/" + mKey] = message;
+  updates["projects/" + projectId + "/" + mKey] = true;
 
-    db.ref().update(updates);
+  db.ref().update(updates);
 }
 
 export function upvoteMessage(messageId) {
-    let updates = {};
+  let updates = {};
 
-    db.ref('messages/' + messageId).on('value', snapshot => {
-        updates['messages/' + messageId + '/upvote'] = snapshot.val().upvote + 1;
-    })
+  db.ref("messages/" + messageId).on("value", snapshot => {
+    updates["messages/" + messageId + "/upvote"] = snapshot.val().upvote + 1;
+  });
 
-    db.ref().update(updates);
+  db.ref().update(updates);
 }
 
 export function downvoteMessage(messageId) {
-    let updates = {};
+  let updates = {};
 
-    db.ref('messages/' + messageId).on('value', snapshot => {
-        updates['messages/' + messageId + '/upvote'] = snapshot.val().upvote - 1;
-    })
+  db.ref("messages/" + messageId).on("value", snapshot => {
+    updates["messages/" + messageId + "/upvote"] = snapshot.val().upvote - 1;
+  });
 
-    db.ref().update(updates);
+  db.ref().update(updates);
 }
 
 export function updateMessageStatus(messageId, status) {
-    let updates = {};
+  let updates = {};
 
-    updates['messages/' + messageId + '/status'] = status;
+  updates["messages/" + messageId + "/status"] = status;
 
-    db.ref().update(updates);
+  db.ref().update(updates);
 }
 
 export function getProjects() {
-    let list = []
-    db.ref("projects").on('value',snapshot => {
-        snapshot.forEach(snapshot => {
-            list.push(snapshot.key)
-        })
+  let list = [];
+  db.ref("projects").on("value", snapshot => {
+    snapshot.forEach(snapshot => {
+      list.push(snapshot.key);
     });
-    return list;
+  });
+  return list;
 }
